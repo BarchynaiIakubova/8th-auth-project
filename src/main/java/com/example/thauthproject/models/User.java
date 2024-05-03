dd68f02fa05c19c3612fb64d6bb56f892654362b
Barchynai <barchynai.iakubova@gmail.com>
2024-05-03 22:00:10 +0600

    Initial commit

---
+++ src/main/java/com/example/thauthproject/models/User.java
@@ -0,0 +1,28 @@
+package com.example.thauthproject.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
+    @SequenceGenerator(name = "user_gen", sequenceName = "user_seq", allocationSize = 1)
+    private Long id;
+
+    private String email;
+
+    private String login;
+
+    private String password;
+}
